4a28ae849c85df9abc4d3acc8a9c568b6da4ba12
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-08-25 23:34:44 +0530

    Update install.js

--- install.js
+++ install.js
@@ -1,1 +1,1 @@
-window.addEventListener("load",async()=>{if("serviceWorker"in navigator)try{const registration=await navigator.serviceWorker.register("/sw.js");console.log("ServiceWorker registration successful with scope: ",registration.scope)}catch(err){console.error(err)}});
+window.addEventListener("load",async()=>{if("serviceWorker"in navigator)try{const registration=await navigator.serviceWorker.register("sw.js");console.log("ServiceWorker registration successful with scope: ",registration.scope)}catch(err){console.error(err)}});
