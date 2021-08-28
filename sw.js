399814b5dbff34012dfb0bd7571e339b515558e6
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-08-28 23:05:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-var dataCacheName = "Shivpaper-data";
-var cacheName = "shivpaper";
-var filesToCache = [
-"/",
+var dataCacheName="Shivpaper-data",
+cacheName="shivpaper",
+filesToCache=[
+    "/",
 "/index.html",
 "/wallpaper.html",
 "/smartphone.html",
@@ -13,7 +13,26 @@ var filesToCache = [
 "/js/app.js",
 "/js/navitem.min.js",
 "/js/bootstrap.bundle.min.js",
-"/iconfull.svg",
+"/icon.svg",
+"/icon.png",
+"/css/IconsRound.woff2",
+
+// "/shivpaper/",
+// "/shivpaper/index.html",
+// "/shivpaper/wallpaper.html",
+// "/shivpaper/smartphone.html",
+// "/shivpaper/laptop.html",
+// "/shivpaper/logo.html",
+// "/shivpaper/about.html",
+// "/shivpaper/css/app.css",
+// "/shivpaper/css/bootstrap.min.css",
+// "/shivpaper/css/IconsRound.woff2",
+// "/shivpaper/js/app.js",
+// "/shivpaper/js/navitem.min.js",
+// "/shivpaper/js/bootstrap.bundle.min.js",
+// "/shivpaper/icon.svg",
+// "/shivpaper/icon.png",
+
 ];
 
 self.addEventListener("install",function(e){console.log("[ServiceWorker] Install"),e.waitUntil(caches.open(cacheName).then(function(e){return console.log("[ServiceWorker] Caching app shell"),e.addAll(filesToCache)}))}),self.addEventListener("activate",function(e){return console.log("[ServiceWorker] Activate"),e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==cacheName&&e!==dataCacheName)return console.log("[ServiceWorker] Removing old cache",e),caches.delete(e)}))})),self.clients.claim()}),self.addEventListener("fetch",function(e){console.log("[Service Worker] Fetch",e.request.url),e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))});
