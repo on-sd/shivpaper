b5c8adb2c8f49a25326e1615bb638207f4c956e7
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-08-29 18:58:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,5 +20,5 @@ filesToCache=[
 ];
 
 self.addEventListener("install",function(e){console.log("[ServiceWorker] Install"),e.waitUntil(caches.open(cacheName).then(function(e){return console.log("[ServiceWorker] Caching app shell"),e.addAll(filesToCache)}))});
-// self.addEventListener("activate",function(e){return console.log("[ServiceWorker] Activate"),e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==cacheName&&e!==dataCacheName)return console.log("[ServiceWorker] Removing old cache",e),caches.delete(e)}))})),self.clients.claim()});
-// self.addEventListener("fetch",function(e){console.log("[Service Worker] Fetch",e.request.url),e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))});
+self.addEventListener("activate",function(e){return console.log("[ServiceWorker] Activate"),e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==cacheName&&e!==dataCacheName)return console.log("[ServiceWorker] Removing old cache",e),caches.delete(e)}))})),self.clients.claim()});
+self.addEventListener("fetch",function(e){console.log("[Service Worker] Fetch",e.request.url),e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))});
