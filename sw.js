be100f7b56a761f0b29380bbe684f3618e32831c
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-08-29 18:56:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,6 @@
 var dataCacheName="Shivpaper-data",
 cacheName="shivpaper",
 filesToCache=[
-// "/",
-// "/index.html",
-// "/wallpaper.html",
-// "/smartphone.html",
-// "/laptop.html",
-// "/logo.html",
-// "/about.html",
-// "/css/app.css",
-// "/css/bootstrap.min.css",
-// "/js/app.js",
-// "/js/navitem.min.js",
-// "/js/bootstrap.bundle.min.js",
-// "/icon.svg",
-// "/icon.png",
-// "/css/IconsRound.woff2",
-
  "/shivpaper/",
  "/shivpaper/index.html",
  "/shivpaper/wallpaper.html",
@@ -35,4 +19,6 @@ filesToCache=[
 
 ];
 
-self.addEventListener("install",function(e){console.log("[ServiceWorker] Install"),e.waitUntil(caches.open(cacheName).then(function(e){return console.log("[ServiceWorker] Caching app shell"),e.addAll(filesToCache)}))}),self.addEventListener("activate",function(e){return console.log("[ServiceWorker] Activate"),e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==cacheName&&e!==dataCacheName)return console.log("[ServiceWorker] Removing old cache",e),caches.delete(e)}))})),self.clients.claim()}),self.addEventListener("fetch",function(e){console.log("[Service Worker] Fetch",e.request.url),e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))});
+self.addEventListener("install",function(e){console.log("[ServiceWorker] Install"),e.waitUntil(caches.open(cacheName).then(function(e){return console.log("[ServiceWorker] Caching app shell"),e.addAll(filesToCache)}))});
+// self.addEventListener("activate",function(e){return console.log("[ServiceWorker] Activate"),e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==cacheName&&e!==dataCacheName)return console.log("[ServiceWorker] Removing old cache",e),caches.delete(e)}))})),self.clients.claim()});
+// self.addEventListener("fetch",function(e){console.log("[Service Worker] Fetch",e.request.url),e.respondWith(caches.match(e.request).then(function(t){return t||fetch(e.request)}))});
