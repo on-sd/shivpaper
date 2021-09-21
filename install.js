1e817569f9a70b8ca6220ebe70a2f30216370ce4
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-09-22 00:08:02 +0530

    Update install.js

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 window.addEventListener("load",async()=>{if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('./sw.js');
 };
 var install = document.getElementById("install");
 install.style.display = 'none';
