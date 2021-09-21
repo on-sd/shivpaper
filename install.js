2fe27ee9ffe4b12af653e746983b84b55606dba7
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-09-22 00:09:40 +0530

    Update install.js

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 window.addEventListener("load",async()=>{if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js');
+  navigator.serviceWorker.register('/on-sd/shivpaper/sw.js');
 };
 var install = document.getElementById("install");
 install.style.display = 'none';
